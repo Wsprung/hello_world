52035ad00f77e9dc858299adc92056a9a3958d4e
Wsprung <whitneykin@gmail.com>
2022-01-06 13:57:30 -0500

    Changes aboutMe page to fix CSS issues and reformat

---
+++ website/aboutMe/sketch.js
@@ -0,0 +1,14 @@
+function preload() {
+}
+
+function setup() {
+  //runs once
+  let canvas = createCanvas(windowWidth, windowHeight);
+  canvas.parent('sketch-container');
+}
+
+function draw() {
+}
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+}
