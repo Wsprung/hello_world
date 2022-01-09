12cf24610cf9592e0361ca6f16b61d56e364d4e2
Wsprung <whitneykin@gmail.com>
2022-01-08 23:45:53 -0500

    Adds food photos, adds background to food page

--- website/myFood/sketch.js
+++ website/myFood/sketch.js
@@ -1,7 +1,32 @@
+let fork;
+let chef;
+let paella;
+
+function preload() {
+  fork = loadImage('../assets/fork.png');
+  chef = loadImage('../assets/chef2.png');
+}
+
 function setup() {
-  createCanvas(400, 400);
+  let canvas = createCanvas(windowWidth, windowHeight);
+  canvas.parent('sketch-container');
+  // background(30, 40, 80);
 }
 
 function draw() {
-  background(220);
+  cursor('../assets/fork.png');
+
+  var numSquares = windowWidth/chef.width;
+
+//variable for the side length of each square
+  var sideLen = windowWidth/numSquares;
+//iterates in y-direction to create rows in the y-direction
+ //var placeGif = true;
+  for(var y = 0; y < windowHeight; y = y + sideLen + 10) {
+ //iterates in x-direction to create row of squares
+  for(var x = 0; x < windowWidth; x = x + sideLen) {
+     image(chef, x, y);
+   }
+  }
+
 }
