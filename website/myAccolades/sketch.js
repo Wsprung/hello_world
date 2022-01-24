ae6ef1e7192093fae15a47c766a2ee16edfc785e
Wsprung <whitneykin@gmail.com>
2022-01-24 16:06:37 -0500

    Testing

--- website/myAccolades/sketch.js
+++ website/myAccolades/sketch.js
@@ -19,6 +19,7 @@ function draw() {
   textSize(20);
   text(message, 10, 80, 270, 80);
   nmsDescript();
+  // ccDescript();
 }
 function windowResized() {
   resizeCanvas(windowWidth, windowHeight + backgroundImg.height - 30);
@@ -40,7 +41,14 @@ function backgroundImgPrint() {
 
 function nmsDescript() {
   var nmsDescriptBounds = document.getElementById('articleIconLight').getBoundingClientRect();
-  if(mouseX < nmsDescriptBounds.right && mouseX > nmsDescriptBounds.left && mouseY < nmsDescriptBounds.top && mouseY > nmsDescriptBounds.bottom) {
+  var right = nmsDescriptBounds.right;
+  var left = nmsDescriptBounds.left;
+  var top = nmsDescriptBounds.top;
+  var bottom = nmsDescriptBounds.bottom;
+console.log(right + "  " + left + "  " + top + "  " + bottom);
+if(mouseX < right + 0 && mouseX > left + 0 && mouseY < bottom + 0 && mouseY > top + 0) {
+
+  // if(mouseX < 1676 && mouseX > 1311 && mouseY < 440 && mouseY > 159) {
     document.getElementById('nmsDescript').style.display = 'inline';
     document.getElementById('articleIconLight').style.display = 'none';
   }
@@ -49,3 +57,15 @@ function nmsDescript() {
     document.getElementById('articleIconLight').style.display = 'inline';
   }
 }
+
+// function ccDescript() {
+//   var ccDescriptBounds = document.getElementById('ccDescript').getBoundingClientRect();
+//   if(mouseX < ccDescriptBounds.right && mouseX > ccDescriptBounds.left && mouseY < ccDescriptBounds.top && mouseY > ccDescriptBounds.bottom) {
+//     document.getElementById('ccDescript').style.display = 'inline';
+//     document.getElementById('articleIcon').style.display = 'none';
+//   }
+//   else {
+//     document.getElementById('ccDescript').style.display = 'none';
+//     document.getElementById('articleIcon').style.display = 'inline';
+//   }
+// }
