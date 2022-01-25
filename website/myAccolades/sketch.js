7c4bc4cfeda009be156e8cf4f6ccd97c47b7a367
Wsprung <whitneykin@gmail.com>
2022-01-24 21:38:34 -0500

    Adds hackathon and SIP icons

--- website/myAccolades/sketch.js
+++ website/myAccolades/sketch.js
@@ -6,7 +6,7 @@ function preload() {
   message = "Expand your screen for the best experience!";
 }
 function setup() {
-  let canvas = createCanvas(windowWidth, windowHeight + backgroundImg.height - 30);
+  let canvas = createCanvas(windowWidth, backgroundImg.height * 3.5);
   canvas.parent('sketch-container');
   // createCanvas(windowWidth, windowHeight + backgroundImg.height - 30);
 }
@@ -22,7 +22,7 @@ function draw() {
   ccDescript();
 }
 function windowResized() {
-  resizeCanvas(windowWidth, windowHeight + backgroundImg.height - 30);
+  resizeCanvas(windowWidth, backgroundImg.height * 3.5);
 }
 function backgroundImgPrint() {
     var numSquares = windowWidth/backgroundImg.width;
@@ -31,7 +31,7 @@ function backgroundImgPrint() {
   //variable for the length of each row
     var sideLen = windowWidth/numSquares;
   //iterates in y-direction to create rows in the y-direction
-    for(var y = 0; y < backgroundImg.height * 4; y = y + backgroundImg.height) {
+    for(var y = 0; y < backgroundImg.height * 4.5; y = y + backgroundImg.height) {
    //iterates in x-direction to create a bookshelf row
     for(var x = 0; x < windowWidth; x = x + sideLen) {
      image(backgroundImg, x, y);
